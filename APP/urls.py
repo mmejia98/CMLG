5c1c548551a787fb036479ca7ee6e69133e8bae2
alessandro66 <calebescoto1@gmail.com>
2022-06-16 23:27:20 -0600

    arreglando importaciones en views

--- APP/urls.py
+++ APP/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ast import Import
 from django.contrib import admin
 from django.urls import path
-from . import views
+from APP.views import inicio, index
+
+#Agregar las 
 
 urlpatterns = [
+    path('inicio/', inicio),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', index, name='index')
+    
 ]
--- APP/views.py
+++ APP/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1 style="text-align:center;">Paguina inicio</h1>')
+    return HttpResponse('<h1 style="text-align:center;">Pagina inicio</h1>')
+
+def inicio(request):
+    return HttpResponse('<h1>pene</h1>')
